ab58139dab9423bcc3d5724ef168b61533bac243
Aime Magnifique NDAYISHIMIYE <82208013+AIMEMAGNI@users.noreply.github.com>
2022-11-24 14:43:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from imp import load_module
+# from imp import load_module
 import streamlit as st
 # from streamlit_option_menu import option_menu
 # from load_image import load_image
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from PIL import Image
+# from PIL import Image
 import base64
 from pandas_profiling import ProfileReport
-from tensorflow import keras
+# from tensorflow import keras
 # import h5py
 import pickle
 
