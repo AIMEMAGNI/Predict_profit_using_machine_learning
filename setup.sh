035d02a207c89a31cadee2a0d05aeee0c5bc6366
Aime Magnifique NDAYISHIMIYE <82208013+AIMEMAGNI@users.noreply.github.com>
2022-11-24 17:05:04 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,11 +6,6 @@ email = \"aimemagnifique01@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-
 [theme]\n\
 base='dark'\n\
 " > ~/.streamlit/config.toml
