1cc6b567eb999bda4210ff49d98c306a9f76ec4f
Aime Magnifique NDAYISHIMIYE <82208013+AIMEMAGNI@users.noreply.github.com>
2022-11-24 14:49:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # from imp import load_module
 import streamlit as st
-# from streamlit_option_menu import option_menu
+from streamlit_option_menu import option_menu
 # from load_image import load_image
 import numpy as np
 import pandas as pd
