673529e648f5388293685e4e5d298940bb05eef3
Aime Magnifique NDAYISHIMIYE <82208013+AIMEMAGNI@users.noreply.github.com>
2022-11-30 15:40:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ if selected == "Project":
     st.write("In this project we used Logistic Regression model.")
     
     st.subheader(f"2. Model accuracy")
-    st.write("The overall accuracy of the model is "+ str(int(0.8982240277293291)))
+    st.write("The overall accuracy of the model is "+ str(float(0.8982240277293291)))
     
     
     
